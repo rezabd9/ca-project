1b79b383100d74beab5fe7f6b09abb47e9810651
rezabd9 <rezabd9@gmail.com>
2019-07-05 13:31:08 +0200

    myscript.py in yml file

---
+++ myscript.py
@@ -0,0 +1,7 @@
+# The entry point to our application
+# Simply import and run our application
+from app import app
+app.run(host='0.0.0.0',debug=True)
+
+sudo pip install -r requirements.txt
+                   python tests.py
