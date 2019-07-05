86728a11181676e9e3b5013926ebc3afb49b98af
rezabd9 <rezabd9@gmail.com>
2019-07-05 13:37:01 +0200

    myscript.py in yml file

--- myscript.py
+++ myscript.py
@@ -1,5 +1,3 @@
-# The entry point to our application
-# Simply import and run our application
-from app import app
-app.run(host='0.0.0.0',debug=True)
-                   python tests.py
+
+import subprocess
+subprocess.call(["python tests.py"])
